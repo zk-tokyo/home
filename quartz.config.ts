c0e836e300b578c5091d105de18b2ccc748c6ed9
adust09 <shoukitsuda@icloud.com>
2025-01-19 16:54:28 +0900

    remove icon

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 zk Tokyo",
+    pageTitle: "zk Tokyo",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,

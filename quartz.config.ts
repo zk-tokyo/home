60c9c163a6c1658c206fec10b98c8aaa4babb6ab
adust09 <shoukitsuda@icloud.com>
2025-01-19 17:35:51 +0900

    Quartz sync: Jan 19, 2025, 5:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://zk-tokyo.github.io/home/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
